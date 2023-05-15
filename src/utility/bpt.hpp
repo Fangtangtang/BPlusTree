829f653fff07160f293de3d0aa839609546696c3
Fangtangtang <2167295472@qq.com>
2023-05-15 20:44:58 +0800

    at last 178

--- src/utility/bpt.hpp
+++ src/utility/bpt.hpp
@@ -10,8 +10,8 @@
 template<class Key, class Value, class Compare1, class Compare2, class Compare3>
 class BPlusTree {
 private:
-    static constexpr int node_size = 180;
-    static constexpr int block_size = 1024;
+    static constexpr int node_size = 178;
+    static constexpr int block_size = 1200;
 
     /*
      * ele in node and block
